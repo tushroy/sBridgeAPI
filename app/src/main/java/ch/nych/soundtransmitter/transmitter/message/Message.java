2c2da705cd49729a5b47940d4611340ead86e58a
Christian Nydegger <nych@nbcn.home>
2016-04-06 15:51:35 +0200

    Implemented message class

--- app/src/main/java/ch/nych/soundtransmitter/transmitter/message/Message.java
+++ app/src/main/java/ch/nych/soundtransmitter/transmitter/message/Message.java
@@ -1,7 +1,57 @@
 package ch.nych.soundtransmitter.transmitter.message;
 
+import ch.nych.soundtransmitter.transmitter.tone.Tone;
+
 /**
  * Created by nych on 4/6/16.
  */
-public interface Message {
+public class Message {
+    public final static int STATE_ABORT = -1;
+    public final static int STATE_PENDING = 0;
+    public final static int STATE_SENDING = 1;
+    public final static int STATE_SENT = 2;
+
+    private int messageId = -1;
+    private byte[] originalData = null;
+    private byte[] preparedData = null;
+    private Tone[] toneSequence = null;
+    private volatile int state = -1;
+
+    public Message(byte[] data, int messageId) {
+        this.originalData = data;
+        this.messageId = messageId;
+        this.state = Message.STATE_PENDING;
+    }
+
+    public int getMessageId() {
+        return this.messageId;
+    }
+
+    public byte[] getOriginalData() {
+        return this.originalData;
+    }
+
+    public byte[] getPreparedData() {
+        return this.preparedData;
+    }
+
+    public void setPreparedData(byte[] preparedData) {
+        this.preparedData = preparedData;
+    }
+
+    public Tone[] getToneSequence() {
+        return this.toneSequence;
+    }
+
+    public void setToneSequence(Tone[] toneSequence) {
+        this.toneSequence = toneSequence;
+    }
+
+    public int getState() {
+        return this.state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
 }
