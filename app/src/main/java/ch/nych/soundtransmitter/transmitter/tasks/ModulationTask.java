0d49af29f35fac32a6566eca08c86e1531c57c18
nych <nydec2@bfh.ch>
2016-04-06 16:01:49 +0200

    Implemented modulationTask

--- app/src/main/java/ch/nych/soundtransmitter/transmitter/tasks/ModulationTask.java
+++ app/src/main/java/ch/nych/soundtransmitter/transmitter/tasks/ModulationTask.java
@@ -2,6 +2,7 @@ package ch.nych.soundtransmitter.transmitter.tasks;
 
 import ch.nych.soundtransmitter.transmitter.Transmitter;
 import ch.nych.soundtransmitter.transmitter.message.Message;
+import ch.nych.soundtransmitter.transmitter.tone.Tone;
 
 /**
  * Created by nych on 4/6/16.
@@ -12,13 +13,36 @@ public class ModulationTask extends TransmissionTask {
         super(transmitter, message, TransmissionTask.MODULATION_TASK);
     }
 
+    protected void fourStateModulation() {
+        System.out.println("ID:\t" + this.message.getMessageId() + " - prepare tone sequence");
+        Tone[] toneSet = this.transmitter.getToneSet();
+        byte[] preparedData = this.message.getPreparedData();
+        Tone[] toneSequence = new Tone[preparedData.length];
+        int tone = 0;
+
+        for(int i = 0; i < toneSequence.length; i++) {
+            tone = preparedData[i] % 4;
+            tone += i % 2 == 0 ? 5 : 0;
+            toneSequence[i] = toneSet[tone];
+        }
+        this.message.setToneSequence(toneSequence);
+    }
+
     @Override
     protected TransmissionTask getNextTask() {
-        return null;
+        return new SendingTask(this.transmitter, this.message);
     }
 
     @Override
     public void run() {
+        if(this.transmitter.transmissionMode == Transmitter.TWO_STATE_TRANSMITTER) {
 
+        } else if(this.transmitter.transmissionMode == Transmitter.FOUR_STATE_TRANSMITTER) {
+            this.fourStateModulation();
+        } else {
+            //Something really went wrong
+            System.err.println("Take a look at the run method of the modulationtask");
+        }
+        this.transmitterCallback();
     }
 }
