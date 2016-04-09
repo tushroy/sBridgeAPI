2b4665a55e8bf41690d51b9cb594c15b11e9cabb
nych <nydec2@bfh.ch>
2016-04-09 17:29:30 +0200

    updated recording and transformation task

--- app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/recording/RecordingTask.java
+++ app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/recording/RecordingTask.java
@@ -2,18 +2,24 @@ package ch.nych.soundtransmitter.receiver.tasks.recording;
 
 import ch.nych.soundtransmitter.receiver.Receiver;
 import ch.nych.soundtransmitter.receiver.tasks.ReceiverTask;
+import ch.nych.soundtransmitter.receiver.tasks.SampleBuffer;
 
 /**
  * Created by nych on 4/9/16.
  */
 public class RecordingTask extends ReceiverTask {
 
+    private SampleBuffer sampleBuffer = null;
+
     public RecordingTask(Receiver receiver) {
         super(receiver);
+        this.sampleBuffer = this.receiver.getSampleBuffer();
     }
 
     @Override
     public void run() {
-
+        while(!this.shutdown) {
+            //capture audio
+        }
     }
 }
--- app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/transformation/TransformationTask.java
+++ app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/transformation/TransformationTask.java
@@ -2,18 +2,46 @@ package ch.nych.soundtransmitter.receiver.tasks.transformation;
 
 import ch.nych.soundtransmitter.receiver.Receiver;
 import ch.nych.soundtransmitter.receiver.tasks.ReceiverTask;
+import ch.nych.soundtransmitter.receiver.tasks.SampleBuffer;
 
 /**
  * Created by nych on 4/9/16.
  */
 public class TransformationTask extends ReceiverTask {
 
+    private SampleBuffer sampleBuffer = null;
+    private double[] windowFunction = null;
+    private Goertzel[] goertzels = null;
+
     public TransformationTask(Receiver receiver) {
         super(receiver);
+        this.sampleBuffer = this.receiver.getSampleBuffer();
+        this.windowFunction = WindowFunction.getHammingWindow(480);
+        this.initGoertzels();
+    }
+
+    private void initGoertzels() {
+        this.goertzels = new Goertzel[] {
+                new Goertzel(48000, 19600, 480),
+                new Goertzel(48000, 19700, 480),
+                new Goertzel(48000, 19800, 480),
+                new Goertzel(48000, 19900, 480),
+                new Goertzel(48000, 20000, 480),
+                new Goertzel(48000, 20100, 480),
+                new Goertzel(48000, 20200, 480),
+                new Goertzel(48000, 20300, 480),
+                new Goertzel(48000, 20400, 480)
+        };
+    }
+
+    private void processWindow() {
+        //process window
     }
 
     @Override
     public void run() {
-
+        while(!this.shutdown) {
+            this.processWindow();
+        }
     }
 }
