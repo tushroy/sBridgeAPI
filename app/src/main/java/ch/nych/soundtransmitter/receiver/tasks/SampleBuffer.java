1db223cd1afe662996e32613071edcdfa493f2a8
nych <nydec2@bfh.ch>
2016-04-09 18:16:03 +0200

    implemented run() method of recordingTask and added debugging log to samplebuffer

--- app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/SampleBuffer.java
+++ app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/SampleBuffer.java
@@ -1,5 +1,7 @@
 package ch.nych.soundtransmitter.receiver.tasks;
 
+import android.util.Log;
+
 import java.util.Arrays;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -25,23 +27,33 @@ public class SampleBuffer {
     }
 
     private void shiftLeft(final int numberOfSamples) {
+        Log.d("MyTag", "\tBuffer overflow, shift left");
         int amount = 0;
         if(numberOfSamples < this.head) {
             amount = this.head;
         } else {
             amount = this.tail - this.sampleBuffer.length + numberOfSamples;
+            Log.w("MyTag", "Buffer size to low, lost: " + (numberOfSamples - this.head) + " samples");
         }
 
         for(int i = amount; i < this.tail; i++) {
             this.sampleBuffer[i-this.head] = this.sampleBuffer[i];
         }
-
+        Log.d("MyTag", "\tShifted left");
+        Log.d("MyTag", "\tHead_old: " + this.head);
+        Log.d("MyTag", "\tTail_old: " + this.tail);
         this.tail -= amount;
         this.head = 0;
+        Log.d("MyTag", "\tHead_new: " + this.head);
+        Log.d("MyTag", "\tTail_new: " + this.tail);
     }
 
-    public int addSamples(final short[] samples) {
+    public void addSamples(final short[] samples) {
         this.lock.lock();
+        Log.d("MyTag", "\n--------------------------");
+        Log.d("MyTag", "Added " + samples.length +" samples");
+        Log.d("MyTag", "Head_old:\t" + this.head);
+        Log.d("MyTag", "Tail_old:\t" + this.tail);
         try {
             if((this.tail + samples.length) > this.sampleBuffer.length) {
                 this.shiftLeft(samples.length);
@@ -50,19 +62,31 @@ public class SampleBuffer {
             for(int i = 0; i < samples.length; i++) {
                 this.sampleBuffer[this.tail++] = samples[i];
             }
+
+            Log.d("MyTag", "Head_new: " + this.head);
+            Log.d("MyTag", "Tail_new: " + this.tail);
+            Log.d("MyTag", "-----------------------------");
         } finally {
             this.lock.unlock();
         }
-        return 0;
     }
 
     public short[] getNextWindow() {
         this.lock.lock();
         short[] window = null;
         try {
+            Log.d("MyTag", "\n--------------------------");
             if((this.head + 480) <= this.tail) {
+                Log.d("MyTag", "Get next window");
+                Log.d("MyTag", "Head_old: " + this.head);
+                Log.d("MyTag", "Tail_old: " + this.tail);
                 window = Arrays.copyOfRange(this.sampleBuffer, this.head, this.head + 480);
                 this.head += 480;
+                Log.d("MyTag", "Head_new: " + this.head);
+                Log.d("MyTag", "Tail_new: " + this.tail);
+                Log.d("MyTag", "--------------------------");
+            } else {
+                Log.d("MyTag", "No Window available at the moment");
             }
         } finally {
             this.lock.unlock();
--- app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/recording/RecordingTask.java
+++ app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/recording/RecordingTask.java
@@ -1,5 +1,13 @@
 package ch.nych.soundtransmitter.receiver.tasks.recording;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import ch.nych.soundtransmitter.receiver.Receiver;
 import ch.nych.soundtransmitter.receiver.tasks.ReceiverTask;
 import ch.nych.soundtransmitter.receiver.tasks.SampleBuffer;
@@ -11,15 +19,40 @@ public class RecordingTask extends ReceiverTask {
 
     private SampleBuffer sampleBuffer = null;
 
+    private int audioSource = MediaRecorder.AudioSource.MIC;
+    private int sampleRateInHz = 48000;
+    private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private int bufferSizeInShorts = 960;
+
+    short buffer[] = new short[bufferSizeInShorts];
+    AudioRecord audioRecorder = null;
+
     public RecordingTask(Receiver receiver) {
         super(receiver);
         this.sampleBuffer = this.receiver.getSampleBuffer();
+        this.audioRecorder = new AudioRecord(
+                this.audioSource,
+                this.sampleRateInHz,
+                this.channelConfig,
+                this.audioFormat,
+                this.bufferSizeInShorts);
     }
 
     @Override
     public void run() {
-        while(!this.shutdown) {
-            //capture audio
+        Log.i("MyTag", "Start Recording Task");
+        if(this.audioRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
+            this.audioRecorder.startRecording();
+        } else {
+            //// TODO: 4/9/16
+            return;
         }
+        while(!this.shutdown) {
+            this.audioRecorder.read(buffer, 0, bufferSizeInShorts);
+            this.sampleBuffer.addSamples(this.buffer);
+        }
+        this.audioRecorder.stop();
+        this.audioRecorder.release();
     }
 }
