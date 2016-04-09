b78390a556dc38e3f7b46827ef45b43ebccfc66d
nych <nydec2@bfh.ch>
2016-04-09 17:35:45 +0200

    implemented samplebuffer

--- app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/SampleBuffer.java
+++ app/src/main/java/ch/nych/soundtransmitter/receiver/tasks/SampleBuffer.java
@@ -1,5 +1,6 @@
 package ch.nych.soundtransmitter.receiver.tasks;
 
+import java.util.Arrays;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -23,13 +24,49 @@ public class SampleBuffer {
         this.lock = new ReentrantLock(true);
     }
 
-    public int addSamples(final short[] samples) {
+    private void shiftLeft(final int numberOfSamples) {
+        int amount = 0;
+        if(numberOfSamples < this.head) {
+            amount = this.head;
+        } else {
+            amount = this.tail - this.sampleBuffer.length + numberOfSamples;
+        }
 
+        for(int i = amount; i < this.tail; i++) {
+            this.sampleBuffer[i-this.head] = this.sampleBuffer[i];
+        }
+
+        this.tail -= amount;
+        this.head = 0;
+    }
+
+    public int addSamples(final short[] samples) {
+        this.lock.lock();
+        try {
+            if((this.tail + samples.length) > this.sampleBuffer.length) {
+                this.shiftLeft(samples.length);
+            }
+
+            for(int i = 0; i < samples.length; i++) {
+                this.sampleBuffer[this.tail++] = samples[i];
+            }
+        } finally {
+            this.lock.unlock();
+        }
         return 0;
     }
 
     public short[] getNextWindow() {
-
-        return null;
+        this.lock.lock();
+        short[] window = null;
+        try {
+            if((this.head + 480) <= this.tail) {
+                window = Arrays.copyOfRange(this.sampleBuffer, this.head, this.head + 480);
+                this.head += 480;
+            }
+        } finally {
+            this.lock.unlock();
+        }
+        return window;
     }
 }
